5fe98105a47f201e018f30dd5f82827f303e9fa4
William Douglas <william.r.douglas@gmail.com>
2016-11-17 21:35:09 +0000

    Try generating certs on the fly

--- trace/tracer_test.go
+++ trace/tracer_test.go
@@ -18,12 +18,15 @@ package trace
 
 import (
 	"flag"
-	"fmt"
+	// "fmt"
+	"io/ioutil"
+	"os"
 	"testing"
 	"time"
 
 	"github.com/01org/ciao/payloads"
 	"github.com/01org/ciao/ssntp"
+	"github.com/01org/ciao/ssntp/certs"
 	"github.com/01org/ciao/testutil"
 )
 
@@ -192,36 +195,76 @@ func validateNewTracer(tracer *Tracer, config TracerConfig, t *testing.T) {
 // NewTracer needs setup for ssntp.Dial (called by dailAndListen)
 type traceMockNotifier struct {}
 func (server *traceMockNotifier) ConnectNotify(uuid string, role ssntp.Role) {
-	fmt.Println("a")
 }
 func (server *traceMockNotifier) DisconnectNotify(uuid string, role ssntp.Role) {
-	fmt.Println("b")
 }
 func (server *traceMockNotifier) StatusNotify(uuid string, status ssntp.Status, frame *ssntp.Frame) {
-	fmt.Println("c")
 }
 func (server *traceMockNotifier) CommandNotify(uuid string, command ssntp.Command, frame *ssntp.Frame) {
-	fmt.Println("d")
 }
 func (server *traceMockNotifier) EventNotify(uuid string, event ssntp.Event, frame *ssntp.Frame) {
-	fmt.Println("e")
 }
 func (server *traceMockNotifier) ErrorNotify(uuid string, error ssntp.Error, frame *ssntp.Frame) {
-	fmt.Println("f")
 }
 
 func newTracerDefaultRunner(part string, t *testing.T) {
 	config := TracerConfig{}
 	configFile := testutil.GetConfigFile("tracer-test")
+	caPath := "tracer-test-test-ca"
+	serverCertPath := "tracer-test-server-test-cert"
+	clientCertPath := "tracer-test-client-test-cert"
 
-	config.UUID = "uuid"
-	cacert, agentCert, err := testutil.RoleToTestCertPath(ssntp.AGENT)
+	template, err := certs.CreateCertTemplate(ssntp.SCHEDULER, "test", "test@test.test", []string{"localhost"}, []string{"127.0.0.1"})
 	if err != nil {
-		t.Error("Missing agent test cert")
+		t.Error("Unable to create cert template")
 		return
 	}
-	config.CAcert = cacert
-	config.Cert = agentCert
+	caWriter, err := os.Create(caPath)
+	if err != nil {
+		t.Error("Unable to create ca file")
+		return
+	}
+	serverCertWriter, err := os.Create(serverCertPath)
+	if err != nil {
+		caWriter.Close()
+		os.Remove(caPath)
+		t.Error("Unable to create server cert file")
+		return
+	}
+	clientCertWriter, err := os.Create(clientCertPath)
+	if err != nil {
+		caWriter.Close()
+		os.Remove(caPath)
+		serverCertWriter.Close()
+		os.Remove(serverCertPath)
+		t.Error("Unable to create client cert file")
+		return
+	}
+	defer caWriter.Close()
+	defer os.Remove(caPath)
+	defer serverCertWriter.Close()
+	defer os.Remove(serverCertPath)
+	defer clientCertWriter.Close()
+	defer os.Remove(clientCertPath)
+
+	err = certs.CreateServerCert(template, false, serverCertWriter, caWriter)
+	if err != nil {
+		t.Error("Unable to create server cert")
+		return
+	}
+	serverCert, err := ioutil.ReadFile(serverCertPath)
+	if err != nil {
+		t.Error("Unable to read server cert")
+		return
+	}
+	err = certs.CreateClientCert(template, false, serverCert, clientCertWriter)
+	if err != nil {
+		t.Error("Unable to create client cert")
+	}
+
+	config.UUID = "uuid"
+	config.CAcert = caPath
+	config.Cert = clientCertPath
 	config.Component = SSNTP
 	config.Spanner = testSpan{}
 	config.Port = 1
@@ -235,14 +278,9 @@ func newTracerDefaultRunner(part string, t *testing.T) {
 	} else if part == "collectoruri" {
 		config.CollectorURI = ""
 	}
-
-	cacert, schedulerCert, err := testutil.RoleToTestCertPath(ssntp.SCHEDULER)
-	if err != nil {
-		t.Error("Missing scheduler test cert")
-	}
 	ssntpConfig := ssntp.Config{}
-	ssntpConfig.CAcert = cacert
-	ssntpConfig.Cert = schedulerCert
+	ssntpConfig.CAcert = caPath
+	ssntpConfig.Cert = serverCertPath
 	ssntpConfig.ConfigURI = configFile
 	ssntpConfig.UUID = "suuid"
 	ssntpConfig.Log = ssntp.Log
@@ -278,43 +316,43 @@ func TestNewTracerDefaultCollectorURI(t *testing.T) {
 	newTracerDefaultRunner("collectoruri", t)
 }
 
-func TestPushSpan(t *testing.T) {
-	config := TracerConfig{}
-	configFile := testutil.GetConfigFile("tracer-test")
+// func TestPushSpan(t *testing.T) {
+// 	config := TracerConfig{}
+// 	configFile := testutil.GetConfigFile("tracer-test")
 
-	config.UUID = "uuid"
-	cacert, agentCert, err := testutil.RoleToTestCertPath(ssntp.AGENT)
-	if err != nil {
-		t.Error("Missing agent test cert")
-		return
-	}
-	config.CAcert = cacert
-	config.Cert = agentCert
+// 	config.UUID = "uuid"
+// 	cacert, agentCert, err := testutil.RoleToTestCertPath(ssntp.AGENT)
+// 	if err != nil {
+// 		t.Error("Missing agent test cert")
+// 		return
+// 	}
+// 	config.CAcert = cacert
+// 	config.Cert = agentCert
 
-	cacert, schedulerCert, err := testutil.RoleToTestCertPath(ssntp.SCHEDULER)
-	if err != nil {
-		t.Error("Missing scheduler test cert")
-	}
-	ssntpConfig := ssntp.Config{}
-	ssntpConfig.CAcert = cacert
-	ssntpConfig.Cert = schedulerCert
-	ssntpConfig.ConfigURI = configFile
-	ssntpConfig.UUID = "suuid"
-	serv := ssntp.Server{}
-	serv.ServeThreadSync(&ssntpConfig, &traceMockNotifier{})
+// 	cacert, schedulerCert, err := testutil.RoleToTestCertPath(ssntp.SCHEDULER)
+// 	if err != nil {
+// 		t.Error("Missing scheduler test cert")
+// 	}
+// 	ssntpConfig := ssntp.Config{}
+// 	ssntpConfig.CAcert = cacert
+// 	ssntpConfig.Cert = schedulerCert
+// 	ssntpConfig.ConfigURI = configFile
+// 	ssntpConfig.UUID = "suuid"
+// 	serv := ssntp.Server{}
+// 	serv.ServeThreadSync(&ssntpConfig, &traceMockNotifier{})
 
-	tracer, _, err := NewTracer(&config)
-	if err != nil {
-		t.Error("Error creating New Tracer")
-		return
-	}
+// 	tracer, _, err := NewTracer(&config)
+// 	if err != nil {
+// 		t.Error("Error creating New Tracer")
+// 		return
+// 	}
 
-	span := payloads.Span{"uuid", "puuid", "cuuid", time.Time{}, "comp", []byte{0}, "msg"}
-	err = tracer.pushSpan(span)
-	if err != nil {
-		t.Error("Error running pushSpan")
-	}
+// 	span := payloads.Span{"uuid", "puuid", "cuuid", time.Time{}, "comp", []byte{0}, "msg"}
+// 	err = tracer.pushSpan(span)
+// 	if err != nil {
+// 		t.Error("Error running pushSpan")
+// 	}
 
-	tracer.Stop()
-	serv.Stop()
-}
+// 	tracer.Stop()
+// 	serv.Stop()
+// }
